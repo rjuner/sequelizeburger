1b300079b31653938d35e4337f4c0b1e29d67880
Rhea Rivera <rjuner@gmail.com>
2016-08-05 23:12:03 -0500

    css working

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 var express = require('express');
-var app = express(); 
 var bodyParser = require('body-parser');
-var models = require('./models');
 var methodOverride = require('method-override');
+
+var app = express(); 
+var models = require('./models');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(methodOverride('_method'));
 
-
 var PORT = process.env.PORT || 3030; 
-
-//var methodOverride = require('method-override');
 
 var exphbs = require('express-handlebars'); 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars'); 
-app.use(bodyParser.urlencoded({extended: false}));
 
 
 app.get('/theburgs', function(req, res){
